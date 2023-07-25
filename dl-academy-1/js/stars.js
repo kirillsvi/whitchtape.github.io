dfb4400695f4601cf64468346fccd3f7975f3de0
kirillsvi <114000817+kirillsvi@users.noreply.github.com>
2023-07-26 01:53:36 +0400

    up30

--- dl-academy-1/js/stars.js
+++
@@ -1,9 +0,0 @@
-const ratingItemsList = document.querySelectorAll('.footer-of-footer__rating-item');
-const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
-
-ratingItemsArray.forEach(item =>
-    item.addEventListener('click', () => {
-        const { itemValue } = item.dataset
-        item.parentNode.dataset.totalValue = item.dataset.itemValue
-    }) 
-);
